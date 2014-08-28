053056097659e651f84be38f4c46cace6045e190
ADSI04 <ADSI04@10.100.113.26>
2014-08-28 23:04:52 +0200

    agrego comentario

---
+++ models/postModel.php
@@ -0,0 +1,17 @@
+<?php
+
+class postModel extends Model
+{
+    public function __construct() {
+        parent::__construct();
+    }
+    
+    public function getPosts()
+    {
+        $post = $this->_db->query("select * from posts");
+        return $post->fetchall();
+    }
+}
+
+
+?>
---
+++ views/layout/default/footer.php
@@ -0,0 +1,8 @@
+
+<div id="footer">
+    copyright &COPY; 2014<?php echo APP_COMPANY;  ?>
+    
+</div>
+
+</body>
+</html>
